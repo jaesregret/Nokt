98add1cd2a838c0b8837948e47e214909d618798
Regret <oisajae@gmail.com>
2026-09-05 16:15:54 -0300

    Create Interpreter.cs

---
+++ src/nk/Interpreter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Nokt;
+
+public class Interpreter
+{
+    public void Execute(List<Statement> statements)
+    {
+        foreach (var statement in statements)
+        {
+            ExecuteStatement(statement);
+        }
+    }
+
+    private void ExecuteStatement(Statement statement)
+    {
+        switch (statement)
+        {
+            case SayStatement say:
+                Console.WriteLine(say.Message);
+                break;
+
+            default:
+                throw new NoktException("Unknown Statement type");
+        }
+    }
+}
+C#
